c7d2b2ae865e3517770e8c670e3dac292c0e4575
Bryce Monaco <brycedmonaco@gmail.com>
2019-01-29 16:49:21 -0800

    Wrote As0

--- As0/src/TutorialApplication.cpp
+++ As0/src/TutorialApplication.cpp
@@ -31,14 +31,38 @@ void TutorialApplication::createScene(void)
 {
     // Create your scene here :)
 	mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+	mCamera->setPosition(0, 47, 222);
 
 	Ogre::Entity* ogreEntity = mSceneMgr->createEntity("ogrehead.mesh");
 
 	Ogre::SceneNode* ogreNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
 	ogreNode->attachObject(ogreEntity);
+	ogreNode->showBoundingBox(true);
 
 	Ogre::Light* light = mSceneMgr->createLight("MainLight");
 	light->setPosition(20, 80, 50);
+
+	Ogre::Entity* ogreEntity2 = mSceneMgr->createEntity("ogrehead.mesh");
+
+	Ogre::SceneNode* ogreNode2 = mSceneMgr->getRootSceneNode()->createChildSceneNode(Ogre::Vector3(84, 48, 0));
+	ogreNode2->attachObject(ogreEntity2);
+	ogreNode2->showBoundingBox(true);
+
+	Ogre::Entity* ogreEntity3 = mSceneMgr->createEntity("ogrehead.mesh");
+
+	Ogre::SceneNode* ogreNode3 = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+	ogreNode3->setPosition(0, 104, 0);
+	ogreNode3->setScale(2, 1.2, 1);
+	ogreNode3->attachObject(ogreEntity3);
+	ogreNode3->showBoundingBox(true);
+
+	Ogre::Entity* ogreEntity4 = mSceneMgr->createEntity("ogrehead.mesh");
+
+	Ogre::SceneNode* ogreNode4 = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+	ogreNode4->setPosition(-84, 48, 0);
+	ogreNode4->roll(Ogre::Degree(-90));
+	ogreNode4->attachObject(ogreEntity4);
+	ogreNode4->showBoundingBox(true);
 
 }
 //---------------------------------------------------------------------------

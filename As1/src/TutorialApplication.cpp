0da3641408cb6c880c93ddd5489931843b22348a
Bryce Monaco <brycedmonaco@gmail.com>
2019-02-10 12:46:06 -0800

    Completed base requirements for As1

--- As1/src/TutorialApplication.cpp
+++ As1/src/TutorialApplication.cpp
@@ -25,19 +25,37 @@ TutorialApplication::~TutorialApplication()
 {
 }
 
+float surfaceHeight = 0;
+
 void TutorialApplication::createScene()
 {
-  mSceneMgr->setAmbientLight(Ogre::ColourValue(.25, .25, .25));
+	mSceneMgr->setSkyBox(true, "Examples/StormySkyBox");
 
-  Ogre::Light* pointLight = mSceneMgr->createLight("PointLight");
-  pointLight->setType(Ogre::Light::LT_POINT);
-  pointLight->setPosition(250, 150, 250);
-  pointLight->setDiffuseColour(Ogre::ColourValue::White);
-  pointLight->setSpecularColour(Ogre::ColourValue::White);
+	mCamera->setPosition(0, 50, 1000); // Just so that the camera does not spawn inside of the sphere
 
-  Ogre::Entity* ninjaEntity = mSceneMgr->createEntity("ninja.mesh");
-  Ogre::SceneNode* ninjaNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("NinjaNode");
-  ninjaNode->attachObject(ninjaEntity);
+
+	//Create the plane
+	Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
+	Ogre::MeshManager::getSingleton().createPlane("ground", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plane, 10000, 10000, 200, 200, true, 1, 5, 5, Ogre::Vector3::UNIT_Z);
+	Ogre::Entity* groundEntity = mSceneMgr->createEntity("ground");
+	Ogre::SceneNode* groundNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("GroundNode");
+	groundNode->attachObject(groundEntity);
+	groundNode->setPosition(0, surfaceHeight, 0);
+	groundEntity->setCastShadows(false);
+	groundEntity->setMaterialName("Examples/Rockwall");
+
+
+	mSceneMgr->setAmbientLight(Ogre::ColourValue(.25, .25, .25));
+
+	Ogre::Light* pointLight = mSceneMgr->createLight("PointLight");
+	pointLight->setType(Ogre::Light::LT_POINT);
+	pointLight->setPosition(250, 150, 250);
+	pointLight->setDiffuseColour(Ogre::ColourValue::White);
+	pointLight->setSpecularColour(Ogre::ColourValue::White);
+
+	Ogre::Entity* sphereEntity = mSceneMgr->createEntity("sphere.mesh");
+	Ogre::SceneNode* sphereNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("SphereNode");
+	sphereNode->attachObject(sphereEntity);
 
 }
 
@@ -52,10 +70,11 @@ bool TutorialApplication::frameRenderingQueued(const Ogre::FrameEvent& fe)
   return ret;
 }
 
+Ogre::Vector3 velocityVec = Ogre::Vector3::ZERO;
+
 bool TutorialApplication::processUnbufferedInput(const Ogre::FrameEvent& fe)
 {
 	static bool mouseDownLastFrame = false;
-	static Ogre::Real toggleTimer = 0.0;
 	static Ogre::Real rotate = .13;
 	static Ogre::Real move = 250;
 
@@ -69,39 +88,65 @@ bool TutorialApplication::processUnbufferedInput(const Ogre::FrameEvent& fe)
 
 	mouseDownLastFrame = leftMouseDown;
 
-	Ogre::Vector3 dirVec = Ogre::Vector3::ZERO;
-
-	if (mKeyboard->isKeyDown(OIS::KC_I))
-	  dirVec.z -= move;
-
-	if (mKeyboard->isKeyDown(OIS::KC_K))
-	  dirVec.z += move;
-
-	if (mKeyboard->isKeyDown(OIS::KC_U))
-	  dirVec.y += move;
-
-	if (mKeyboard->isKeyDown(OIS::KC_O))
-	  dirVec.y -= move;
-
-	if (mKeyboard->isKeyDown(OIS::KC_J))
+	if (mKeyboard->isKeyDown(OIS::KC_NUMPAD8)) // Forward
 	{
-	  if(mKeyboard->isKeyDown(OIS::KC_LSHIFT))
-	    mSceneMgr->getSceneNode("NinjaNode")->yaw(Ogre::Degree(5 * rotate));
-	  else
-	    dirVec.x -= move;
+		velocityVec.z -= move;
+
 	}
 
-	if (mKeyboard->isKeyDown(OIS::KC_L))
+	if (mKeyboard->isKeyDown(OIS::KC_NUMPAD2)) // Backward
 	{
-	  if(mKeyboard->isKeyDown(OIS::KC_LSHIFT))
-	    mSceneMgr->getSceneNode("NinjaNode")->yaw(Ogre::Degree(-5 * rotate));
-	  else
-	    dirVec.x += move;
+		velocityVec.z += move;
+
 	}
 
-	mSceneMgr->getSceneNode("NinjaNode")->translate(
-	  dirVec * fe.timeSinceLastFrame,
-	  Ogre::Node::TS_LOCAL);
+	if (mKeyboard->isKeyDown(OIS::KC_NUMPAD7)) // Up, Assignment requires PGUP, but the built in camera uses that already
+	{
+		velocityVec.y += move;
+
+	}
+
+	if (mKeyboard->isKeyDown(OIS::KC_NUMPAD9)) // Down, Assignment requires PGDOWN, but the built in camera uses that already
+	{
+		velocityVec.y -= move;
+
+	}
+
+	if (mKeyboard->isKeyDown(OIS::KC_NUMPAD4)) // Left
+	{
+		velocityVec.x -= move;
+
+	}
+
+	if (mKeyboard->isKeyDown(OIS::KC_NUMPAD6)) // Right
+	{
+		velocityVec.x += move;
+
+	}
+
+	if (mKeyboard->isKeyDown(OIS::KC_SPACE)) // Stop the motion
+	{
+		velocityVec = Ogre::Vector3::ZERO;
+
+	}
+
+	// Apply the velocity vector
+	mSceneMgr->getSceneNode("SphereNode")->translate(velocityVec * fe.timeSinceLastFrame, Ogre::Node::TS_LOCAL);
+
+	//Ground plane movement
+	if (mKeyboard->isKeyDown(OIS::KC_MINUS)) // Down
+	{
+		surfaceHeight -= 1;
+
+	}
+
+	if (mKeyboard->isKeyDown(OIS::KC_EQUALS)) // Up
+	{
+		surfaceHeight += 1;
+
+	}
+
+	mSceneMgr->getSceneNode("GroundNode")->setPosition(0, surfaceHeight, 0);
 
 	return true;
 }

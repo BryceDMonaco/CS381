983c90b7a849f0d3c1313fd14e47d3eca277a176
Bryce Monaco <brycedmonaco@gmail.com>
2019-03-15 11:15:37 -0700

    Created As5, implemented altitude controls

---
+++ As5/inc/GfxMgr.h
@@ -0,0 +1,41 @@
+#ifndef INC_GFXMGR_H_
+#define INC_GFXMGR_H_
+
+#include "Mgr.h"
+
+#include <OgreRoot.h>
+#include <OgreConfigFile.h>
+#include "OgreRenderWindow.h"
+#include <OgreSceneManager.h>
+#include <OgreCamera.h>
+#include "OgreViewport.h"
+#include <OgreWindowEventUtilities.h>
+
+
+
+class GfxMgr : public Mgr
+{
+public:
+	GfxMgr (Engine* engine);
+	~GfxMgr ();
+
+	void Init ();
+	void Tick (float dt);
+	void Stop ();
+
+private:
+	Ogre::Root* mRoot;
+	Ogre::String mResourcesCfg;
+	Ogre::String mPluginsCfg;
+
+	Ogre::RenderWindow* mWindow;
+
+	Ogre::SceneManager* mSceneMgr;
+
+	Ogre::Camera* mCamera;
+
+};
+
+
+
+#endif /* INC_GFXMGR_H_ */
